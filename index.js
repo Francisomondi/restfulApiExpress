a5036390394d0acb4c35c90f865de1e1a6fbc5e8
francis omondi <francisomondi17@gmail.com>
2022-10-18 10:20:55 +0300

    returning error messages

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ app.post("/api/courses", (req, res) => {
     const result = schema.validate(req.body);
     console.log(result);
 
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+
 
     const course = {
         id: courses.length + 1,
