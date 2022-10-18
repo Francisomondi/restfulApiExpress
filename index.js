67b928805e57acb22fad0784f88da72544f3a01b
francis omondi <francisomondi17@gmail.com>
2022-10-18 07:56:00 +0300

    handling get requests

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
+
+const courses = [{
+        id: 1,
+        name: "course 1",
+        description: "this is the first course"
+    },
+    {
+        id: 2,
+        name: "course 3",
+        description: "this is the second course"
+    },
+    {
+        id: 3,
+        name: "course 3",
+        description: "this is the third course"
+    }
+];
 
 app.get("/", (req, res) => {
-    res.send("Hello this is our home page")
-})
+    res.send(courses);
+});
 
 app.get("/api/courses", (req, res) => {
-    res.send("route to courses")
-})
+    res.send("route to courses");
+});
 
-const PORT = process.env.PORT || 3000
+app.get("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send("course not found");
+    res.send(course);
+});
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
