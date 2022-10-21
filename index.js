7c8ad43873c89608bbec6826f19d27fccea1842e
francis omondi <francisomondi17@gmail.com>
2022-10-21 08:43:23 +0300

    working with third-party packages

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const Joi = require('joi');
 const express = require("express");
 const logger = require("./logger");
 const auth = require("./auth");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+app.use(helmet());
+app.use(morgan("tiny"));
 app.use(logger);
 app.use(auth);
 
